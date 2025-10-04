9f88d510b89b48c73d5dfb931a48ef57b835f363
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-10-04 16:47:56 +0300

    Konum Yöneticisi

--- app/src/main/java/com/example/excercise4/MapsActivity.kt
+++ app/src/main/java/com/example/excercise4/MapsActivity.kt
@@ -1,5 +1,8 @@
 package com.example.excercise4
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -15,6 +18,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener : LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,21 +33,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(p0: Location) {
+
+            }
+        }
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val eiffel = LatLng(48.85391,2.2913545)
+//        mMap.addMarker(MarkerOptions().position(eiffel))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel,10f))
+
     }
 }
